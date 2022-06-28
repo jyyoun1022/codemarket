938e1f7b3cb156eeff7edc17c31c8df15b156de9
younjaeyeal <jyyoun1021@gmail.com>
2022-06-29 07:34:28 +0900

    Initial commit

---
+++ src/main/java/codej/codemarket/CodemarketApplication.java
@@ -0,0 +1,13 @@
+package codej.codemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodemarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codej/codemarket/CodemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package codej.codemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
